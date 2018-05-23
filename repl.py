dca3275b576c61754cf9f300d9dfc95898718aff
Graham Breed <gbreed@gmail.com>
2018-05-23 20:43:49 +0100

    Make noises according to results

--- repl.py
+++ repl.py
@@ -1,3 +1,4 @@
+import music
 from microbit import display, Image
 
 while True:
@@ -10,12 +11,15 @@ while True:
         break
     except Exception as e:
         display.show(Image.CONFUSED)
+        music.pitch(55, 1000, wait=False)
         print(e)
     else:
         if val is True:
             display.show(Image.HAPPY)
+            music.pitch(528, 300, wait=False)
         elif val is False:
             display.show(Image.SAD)
+            music.pitch(66, 1000, wait=False)
         else:
             display.clear()
         if val is not None:
