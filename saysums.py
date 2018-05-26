049e178b8a2bfe1c6a3a7b1f9b0c71b41722b92e
Graham Breed <gbreed@gmail.com>
2018-05-26 16:15:17 +0100

    Add a plain text-to-speech mode

--- saysums.py
+++ saysums.py
@@ -1,6 +1,6 @@
 import sys
 import speech
-from microbit import display, Image, sleep
+from microbit import display, Image, sleep, button_a
 
 numbers = {
         10: 'ten',
@@ -17,35 +17,40 @@ numbers = {
 
 def sayrepr():
     while True:
-        line = input('>>> ')
-        if not line:
-            continue
-        try:
-            val = eval(line)
-        except EOFError:
-            break
-        except Exception as e:
-            display.show(Image.CONFUSED)
-            sys.print_exception(e)
-            speech.say("I don't understand")
+        while not button_a.was_pressed():
+            command()
+        while not button_a.was_pressed():
+            speech.say(input('? '))
+
+
+def command():
+    line = input('>>> ')
+    if not line:
+        return
+    try:
+        val = eval(line)
+    except Exception as e:
+        display.show(Image.CONFUSED)
+        sys.print_exception(e)
+        speech.say("I don't understand")
+    else:
+        if val is not None:
+            print(str(val))
+            speech.say(line)
+        if val is True:
+            display.show(Image.HAPPY)
+            sleep(100)
+            speech.say("is right")
+        elif val is False:
+            display.show(Image.SAD)
+            sleep(100)
+            speech.say("is false")
+        elif isinstance(val, int):
+            speech.say("equals")
+            speech.say(numbers.get(val, str(val)))
+            display.clear()
         else:
-            if val is not None:
-                print(str(val))
-                speech.say(line)
-            if val is True:
-                display.show(Image.HAPPY)
-                sleep(100)
-                speech.say("is right")
-            elif val is False:
-                display.show(Image.SAD)
-                sleep(100)
-                speech.say("is false")
-            elif isinstance(val, int):
-                speech.say("equals")
-                speech.say(numbers.get(val, str(val)))
-                display.clear()
-            else:
-                display.clear()
+            display.clear()
 
 
 if __name__ == '__main__':
